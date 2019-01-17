8372c9b2efe689750d22dfb047f856329638f2f5
Pilar <pilarcanalsdros@gmail.com>
2019-01-17 12:50:29 +0100

    Removing duplicates 4000 observations

--- sentiment.R
+++ sentiment.R
@@ -8,7 +8,7 @@ options(scipen=999)
 #### ._LIBRARIES####
 
 pacman::p_load(plyr, purrr, BiocManager, ggplot2, rattle, RGtk2, corrplot, 
-               plyr, psych, DataExplorer, caret, arules, randomForest, C50)
+               plyr, psych, DataExplorer, caret, arules, randomForest, C50,dplyr)
 
 
 setwd("C:/Users/pilar/Google Drive/A_DATA/UBIQUM/TASK4.1/Sentiment Analysis")
@@ -89,6 +89,14 @@ anyNA(iphonematrix_all)
 # plot.new()
 # dev.off()
 
+
+####REMOVING DUPLICATES####
+
+#s<-distinct(galaxymatrix_all[, !names(galaxymatrix_all) %in%"id"])
+
+wodup<-galaxymatrix_all%>%select( -id)%>%distinct()
+
+
 ####CORRELATION MATRIX####
 
 # corrplot(cor(iphonematrix_all), order ="hclust")
@@ -116,39 +124,63 @@ newgalaxy<- galaxymatrix_all[, -highlyCorDescr]
 
 corrplot(cor(newgalaxy), order ="hclust")
 
+#withoutduplicates
+corrplot(cor(wodup), order ="hclust")
+descrCor <- cor(wodup)
+summary(descrCor[upper.tri(descrCor)])
+highlyCorDescr <- findCorrelation(descrCor, cutoff = .80) 
+
+
+summary(highlyCorDescr) 
+newwodup<- wodup[, -highlyCorDescr]
+
+corrplot(cor(newwodup), order ="hclust")
+
 #### OPTIMIZATION ####
 #as we are working with samsung products will remove all variables for samsung#
 
-galaxynames<-names(newgalaxy)
 
 samsung<-grep("sam", names(newgalaxy), value = TRUE)
+nodupsamsung<-grep("samsung", names(newwodup), value = TRUE)
+
 nosam<-setdiff(galaxynames, samsung)
+nosapdup<-setdiff(nodupnames, nodupsamsung)
 
 newgalaxy<-newgalaxy[nosam]
+newwodup<-newwodup[nosapdup]
 
 corrplot(cor(newgalaxy), order ="hclust")
-
+corrplot(cor(newwodup), order ="hclust")
 
 #### SELECTING FEATURES####
 
 # will keep the variables more correlated#
-cor(x =  newgalaxy, y =newgalaxy$galaxySentiment)
 
 galcor<-cor(x =  newgalaxy, y =newgalaxy$galaxySentiment)
+dupcor<-cor(x =  newwodup, y =newwodup$galaxySentiment)
 
 galcordf<- as.data.frame(galcor)
+dupcordf<- as.data.frame(dupcor)
 
 namescor<- rownames(galcordf)
+namesdup<-rownames(dupcordf)
 
 galcordf<-abs(galcordf)
+dupcordf<-abs(dupcordf)
 
 galcordf$namescor<-namescor
+dupcordf$namesdup<-namesdup
+
 
 badcorr<-galcordf%>%dplyr::filter(galcordf$V1<.5 )
+baddupcorr<-dupcordf%>%dplyr::filter(dupcordf$V1<.5 )
 
 goodcor<-setdiff(names(newgalaxy), badcorr$namescor)
+gooddupcor<-setdiff(names(newwodup), baddupcorr$namesdup)
+
 
 corrplot(cor(newgalaxy[goodcor]), order ="hclust")
+corrplot(cor(newwodup[gooddupcor]), order ="hclust")
 
 
 
@@ -157,9 +189,12 @@ corrplot(cor(newgalaxy[goodcor]), order ="hclust")
 set.seed(123)
 
 partition<-createDataPartition(newgalaxy$galaxySentiment,times = 1, p = 0.7, list = FALSE)
+partitiondup<-createDataPartition(newwodup$galaxySentiment,times = 1, p = 0.7, list = FALSE)
 
 gala_train<-newgalaxy[partition,]
 gala_test<-newgalaxy[-partition,]
+dup_train<-newgalaxy[partitiondup,]
+dup_test<-newgalaxy[-partitiondup,]
 
 ###SVM###
 
@@ -226,7 +261,12 @@ lm<-train(galaxySentiment~.,
                 data=gala_train[goodcor],
                 method= "lm")
 
+lmdup<-train(galaxySentiment~.,
+          data=dup_train[gooddupcor],
+          method= "lm")
+
 lmpredict<-predict(lm, gala_test)
+lmdupredict<-predict(lmdup, dup_test)
 
 postResample(lmpredict,newgalaxy$galaxySentiment)
 
@@ -269,8 +309,26 @@ disfixed7_lmpredict <- discretize(lmpredict, "fixed",
 
 summary(disfixed7_lmpredict)
 
+#dup#
+
+disfixed7_dup_test <- discretize(dup_test$galaxySentiment, "fixed",
+                                  breaks =c(-Inf, -50, -10, -1, 1, 10, 50, Inf),
+                                  labels = c("very_negative", "negative", "somewhat_negative",
+                                             "neutral", "somewhat_positive", "positive", "very_positive"))
+summary(disfixed7_dup_test)
+
+dup_test$galaxySentiment<-disfixed7_dup_test #replacing for discretize data#
+
+disfixed7_lmdupredict <- discretize(lmdupredict, "fixed",
+                                  breaks =c(-Inf, -50, -10, -1, 1, 10, 50, Inf),
+                                  labels = c("very_negative", "negative", "somewhat_negative",
+                                             "neutral", "somewhat_positive", "positive", "very_positive"))
+
+summary(disfixed7_lmdupredict)
+summary(dup_test$galaxySentiment)
 
 confusionMatrix(disfixed7_lmpredict, gala_test$galaxySentiment) #any improvement#
+confusionMatrix(disfixed7_lmdupredict, dup_test$galaxySentiment) #any improvement#
 
 
 ####NEXT STEPS####
